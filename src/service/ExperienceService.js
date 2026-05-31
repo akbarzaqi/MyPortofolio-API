24cb9df7d374ca2504a2c5eaee6d7e519a91f959
akbarzaqi <zakiakbar143@gmail.com>
2026-05-31 23:28:34 +0700

    fix: remove SSL settings from database connection in service files

--- src/service/ExperienceService.js
+++ src/service/ExperienceService.js
@@ -5,9 +5,6 @@ class ExperienceService {
     constructor() {
         this.Pool = new Pool({
             connectionString: process.env.DATABASE_URL || `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.DB_HOST || 'localhost'}:${process.env.POSTGRES_PORT || 5432}/${process.env.POSTGRES_DB}`,
-            ssl : {
-                rejectUnauthorized: false
-            },
             user: process.env.POSTGRES_USER,
             database: process.env.POSTGRES_DB,
             password: process.env.POSTGRES_PASSWORD,
